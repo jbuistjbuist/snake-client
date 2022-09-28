3e775a9bc528629378a748e50c1e4167a899fe3a
Jeremy Buist <jbuistjbuist@gmail.com>
2022-09-28 13:23:15 -0400

    adding seperate client and play modules

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-//IP: 165.227.47.243 PORT: 50541
-
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port:  50541
-  });
-  //sets the type of encoding
-  conn.setEncoding("utf8");
-  return conn;
-};
+const {connect} = require("./client.js");
 
 const conn = connect();
 
 conn.on("data", (data) => {
   console.log(data);
-})
+});
